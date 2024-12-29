a08610d89bfa61630afec1c444abceb3dcc82d6a
ShabDarya <148251014+ShabDarya@users.noreply.github.com>
2024-12-29 17:55:30 +0300

    Update hw_streamlit.py

--- hw_streamlit.py
+++ hw_streamlit.py
@@ -5,6 +5,7 @@ import statistics
 import plotly.express as px
 import numpy as np 
 import matplotlib.pyplot as plt
+import requirements.txt pip
 
 df_mean_rolling=pd.DataFrame()
 df_stat=pd.DataFrame(columns=['city','season','mean_t','std_t'])
